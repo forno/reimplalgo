401ae64eed8b194ad2c6f7245893758f93092819
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-04-25 03:11:19 +0900

    Add test of integral trapezoidal rule

---
+++ test/integral/trapezoidal_rule.cpp
@@ -0,0 +1,57 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2018, Doi Yusuke
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "reimplalgo/integral/trapezoidal_rule.hpp"
+
+#include <gtest/gtest.h>
+
+using func_signature = double(*)(double);
+
+class TrapezoidalRuleTest
+  : public ::testing::TestWithParam<func_signature>
+{
+protected:
+  virtual ~TrapezoidalRuleTest();
+};
+
+TrapezoidalRuleTest::~TrapezoidalRuleTest() = default;
+
+TEST_P(TrapezoidalRuleTest, NullInputTest)
+{
+  EXPECT_EQ(0, reimplalgo::integral::trapezoidal_rule(GetParam(), 0., 0., 0));
+  EXPECT_EQ(0, reimplalgo::integral::trapezoidal_rule(GetParam(), 0., 0., 1));
+  EXPECT_EQ(0, reimplalgo::integral::trapezoidal_rule(GetParam(), 0., 0., std::numeric_limits<std::size_t>::max()));
+  EXPECT_EQ(0, reimplalgo::integral::trapezoidal_rule(GetParam(), 1., 1., 0));
+  EXPECT_EQ(0, reimplalgo::integral::trapezoidal_rule(GetParam(), -1., -1., 0));
+}
+
+INSTANTIATE_TEST_CASE_P(FunctionsParameterized, TrapezoidalRuleTest, ::testing::Values(
+    [](double e){return 0.;},
+    [](double e){return e;},
+    [](double e){return e/2;},
+    [](double e){return e*e;}));
